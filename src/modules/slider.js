6dc17bb6800303961c3289812245d7f23aaf53a6
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2022-01-18 12:35:44 +0300

    fix: slider

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -9,15 +9,55 @@ export const slider = () => {
         const showSlides = (slides) => {
             slides = services.querySelectorAll(".col-md-12.col-lg-6");
             slides.forEach((slide, index) => {
-                if (document.documentElement.scrollWidth > 576){
+                if (document.documentElement.scrollWidth > 1200){
                     if(index == 0 || index == 1){
                         slide.style.display = "inline";
+                        slide.style.width = "50%";
+                        slide.querySelector("img").style.width = "220px";
+                        slide.querySelector(".service-text").style.marginLeft = "220px";
+                        slide.querySelector("h3").style.fontSize = "";
+                        slide.querySelector(".service-block").style.margin = "";
+                        slide.querySelector(".service-button").style.display = "";
                     } else {
                         slide.style.display = "none";
                     }
-                } else {
+                } else if (document.documentElement.scrollWidth > 991 && document.documentElement.scrollWidth <= 1200){
+                    if(index == 0 || index == 1){
+                        slide.style.display = "inline-block";
+                        slide.style.width = "50%";
+                        slide.querySelector(".service-block").style.margin = "0";
+                        slide.querySelector("h3").style.fontSize = "18px";
+                        slide.querySelector("img").style.width = "150px";
+                        slide.querySelector("img").style.height = "100%";
+                        slide.querySelector(".service-text").style.marginLeft = "150px";
+                    } else {
+                        slide.style.display = "none";
+                    }
+                } else if (document.documentElement.scrollWidth > 576 && document.documentElement.scrollWidth <= 991){
+                    if(index == 0 || index == 1){
+                        slide.style.display = "inline-block";
+                        slide.style.width = "48%";
+                        slide.querySelector(".service-block").style.margin = "0";
+                        slide.querySelector(".service-button").style.display = "none";
+                        slide.querySelector("h3").style.fontSize = "18px";
+                        slide.querySelector("img").style.width = "150px";
+                        slide.querySelector("img").style.height = "100%";
+                        slide.querySelector(".service-text").style.marginLeft = "150px";
+                        if (document.documentElement.scrollWidth <= 767){
+                            slide.querySelector("h3").style.fontSize = "14px";
+                            slide.querySelector("img").style.width = "100px";
+                            slide.querySelector(".service-text").style.marginLeft = "100px";
+                        }
+                    } else {
+                        slide.style.display = "none";
+                    }                    
+                }else {
                     if (index == 0){
                         slide.style.display = "inline";
+                        slide.style.width = "";
+                        slide.querySelector(".service-button").style.display = "";
+                        slide.querySelector("img").style.width = "220px";
+                        slide.querySelector(".service-text").style.marginLeft = "220px";
                     } else {
                         slide.style.display = "none";
                     }
