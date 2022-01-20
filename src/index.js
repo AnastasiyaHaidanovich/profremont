bdf0763d6e05e97f118ec99c8279802f39f4ac29
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2022-01-20 10:47:35 +0300

    fix: modal

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import { modal } from "./modules/modal";
 import { timer } from './modules/timer';
 import { menu } from './modules/menu';
 
-modal(".btn-warning", ".header-modal", "header-modal__close");
-
-modal(".btn-lg", ".header-modal", "header-modal__close");
-
-modal(".open-modal", ".header-modal", "header-modal__close");
-
-modal(".btn-sm", ".services-modal", "services-modal__close");
-
+modal();
 menu();
 swiper();
 timer('12 january 2022');
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,11 +1,37 @@
 import { animate } from './helpers';
 
-export const modal = (btn, modalClass, closeBtnClass) => {
-    const modal = document.querySelector(modalClass);
+export const modal = () => {
+
+    
+    // modal(".btn-warning", ".header-modal", "header-modal__close");
+
+    // modal(".btn-lg", ".header-modal", "header-modal__close");
+
+    // modal(".open-modal", ".header-modal", "header-modal__close");
+
+    // modal(".btn-sm", ".services-modal", "services-modal__close");
     const overlay = document.querySelector(".overlay");
     const body = document.querySelector("body");
 
-    const smoothClose = () => {
+    const smoothOpen = (modal) => {
+        modal.style.display = "block";
+        modal.style.opacity = 0;
+        overlay.style.display = "block";
+        overlay.style.opacity = 0;
+
+        animate({
+            duration: 1000,
+            timing(timeFraction) {
+            return timeFraction;
+            },
+            draw(progress) {
+            modal.style.opacity = progress;
+            overlay.style.opacity = progress;
+            }
+        });
+    };
+
+    const smoothClose = (modal) => {
         animate({
             duration: 1000,
             timing(timeFraction) {
@@ -23,38 +49,31 @@ export const modal = (btn, modalClass, closeBtnClass) => {
     };
 
     body.addEventListener('click', (e) => {
-        if(e.target.closest(btn)){
+        if(e.target.closest(".btn-warning") || e.target.closest(".btn-lg") || e.target.closest(".open-modal")){
             e.preventDefault();
-            const smoothOpen = () => {
-                modal.style.display = "block";
-                modal.style.opacity = 0;
-                overlay.style.display = "block";
-                overlay.style.opacity = 0;
-
-                animate({
-                    duration: 1000,
-                    timing(timeFraction) {
-                    return timeFraction;
-                    },
-                    draw(progress) {
-                    modal.style.opacity = progress;
-                    overlay.style.opacity = progress;
-                    }
-                });
-            };
             
             if(window.innerWidth > 768){
-                smoothOpen();
+                smoothOpen(document.querySelector(".header-modal"));
             } else {
-                modal.style.display = "block";
+                document.querySelector(".header-modal").style.display = "block";
                 overlay.style.display = "block";
             }
         }
-        if(e.target.classList.contains(closeBtnClass)){
-            if (window.innerWidth > 768){
-                smoothClose();
+        if(e.target.closest(".btn-sm")){
+            e.preventDefault();
+            
+            if(window.innerWidth > 768){
+                smoothOpen(document.querySelector(".services-modal"));
             } else {
-                modal.style.display = "none";
+                document.querySelector(".services-modal").style.display = "block";
+                overlay.style.display = "block";
+            }
+        }
+        if(e.target.classList.contains("header-modal__close") || e.target.classList.contains("services-modal__close")){
+            if (window.innerWidth > 768){
+                smoothClose(e.target.classList.contains("header-modal__close") ? document.querySelector(".header-modal") : document.querySelector(".services-modal"));
+            } else {
+                (e.target.classList.contains("header-modal__close") ? document.querySelector(".header-modal") : document.querySelector(".services-modal")).style.display = "none";
                 overlay.style.display = "none";
             }
         }        
